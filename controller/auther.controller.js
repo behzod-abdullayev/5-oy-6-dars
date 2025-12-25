c7db25ecaab8ca0f40100d7e48e979b5b14ad7fa
behzod07-abdullayev <bekzod2366@gmail.com>
2025-12-25 23:11:56 +0500

    5-oy-6-dars

--- controller/auther.controller.js
+++ controller/auther.controller.js
@@ -1,6 +1,7 @@
 const AutherSchema = require("../schema/auther.schema");
 const BookSchema = require("../schema/book.schema");
 const CustomErrorHandler = require("../utils/custom-error-handler");
+const user = require("../schema/auth.schema")
 
 const getAllAuthors = async (req, res, next) => {
   try {
@@ -135,11 +136,34 @@ const search = async (req, res, next) => {
 };
 
 
+const updateadmin = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const foundUser = await user.findById(id);
+    if (!foundUser) {
+      return res.status(404).json({ message: "user not found" });
+    }
+
+    foundUser.role = "admin";
+    await foundUser.save();
+
+    res.status(200).json({
+      message: `User ${foundUser.name} role updated to admin`,
+      user: foundUser,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
 module.exports = {
   getAllAuthors,
   getOneAuthor,
   addAuthor,
   updateAuthor,
   deleteAuthor,
-  search
+  search,
+  updateadmin
 };
--- router/author.routes.js
+++ router/author.routes.js
@@ -1,6 +1,7 @@
 const {Router} = require("express")
-const { getAllAuthors, addAuthor, getOneAuthor, updateAuthor, deleteAuthor, search } = require("../controller/auther.controller")
+const { getAllAuthors, addAuthor, getOneAuthor, updateAuthor, deleteAuthor, search, updateadmin } = require("../controller/auther.controller")
 const authorValidationMiddleware = require("../middleware/author.validation.middleware")
+const { authorization, authorization2 } = require("../middleware/authorization")
 
 
 const AuthorRouter = Router()
@@ -8,10 +9,12 @@ const AuthorRouter = Router()
 AuthorRouter.get("/get_all_authors", getAllAuthors)
 AuthorRouter.get("/get_one_author/:id", getOneAuthor)
 AuthorRouter.get("/search", search)
-AuthorRouter.post("/add_author", authorValidationMiddleware, addAuthor )
-AuthorRouter.put("/update_author/:id", updateAuthor)
-AuthorRouter.delete("/delete_author/:id", deleteAuthor)
+AuthorRouter.post("/add_author", authorValidationMiddleware, authorization, authorization2, addAuthor )
+AuthorRouter.put("/update_author/:id", authorization, authorization2, updateAuthor)
+AuthorRouter.delete("/delete_author/:id", authorization, authorization2, deleteAuthor)
 
+AuthorRouter.put("/update_user/:id", updateadmin);
 
+// AuthorRouter.put("/update_user/:id", authorization2, updateadmin); bu korinishda yani authorization2 ni ishlatih uchun suoer admin kerak yani agar super admin qoshsangiz va auhorization 2 ni ishlatsangiz faqatgin superadmin role ni ozgartia oladigan bo'ladi
 
 module.exports = AuthorRouter
